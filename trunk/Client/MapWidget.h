0518853dceab91f3320cbb7c8ee10e10c52aebfa
gigotdarnaud <gigotdarnaud@365edff8-7315-e0fa-443c-12cc7387b311>
2011-01-29 18:08:52 +0000

    Map editor started.

--- trunk/Client/MapWidget.h
+++ trunk/Client/MapWidget.h
@@ -8,13 +8,18 @@
 #define MAX_LOADED_RESSOURCES 2000000000
 #define RESSOURCES_FOLDER "Ressources/"
 #define MAP_FOLDER "Maps/"
+#define BLOC_SIZE 24
 
 class QTimer;
-
+class MapEditor;
 
 class MapWidget : public QSFMLCanvas
 {
     Q_OBJECT
+
+    friend class MapEditor;
+    friend class MapWidgetScroll;
+
     public:
 
     MapWidget(QWidget* Parent, const QPoint& Position, const QSize& Size);
@@ -32,6 +37,9 @@ class MapWidget : public QSFMLCanvas
     bool setMap(const QString& fileName) { bool b=(m_map=loadMap(fileName)).get()==NULL;adjustSize(); return b; }
 
     static std::auto_ptr<MapInformations> loadMap(QString fileName);
+    static bool saveMap(const MapInformations*, QString fileName);
+
+    bool saveMap(const QString& fileName) const;
 
     RSID loadRessource(QString fileName); //Return 0, if the image is already loaded it returns it RSID.
     QList<RSID> loadRessourcesPack(QString fileName, bool exclusive=true); //Return the RSID of succefully loaded ressources. If exclusive is set to true, the previous ressources will be freed, and the RSID will restart from 1.
@@ -39,6 +47,8 @@ class MapWidget : public QSFMLCanvas
     QList<RSID> loadRessourcesFolder(QString folderName, bool exclusive=true);
 
     bool isMapValid() const;
+
+    QPoint selectedCase() const { return m_selectedCase; }
 
     public slots:
 
@@ -48,12 +58,17 @@ class MapWidget : public QSFMLCanvas
 
     signals:
 
-    void mapClicked(int caseX, int caseY);
+    void mapClicked(QPoint newCase);
+    void highlightedCaseChanged(QPoint newCase);
 
     protected:
 
     virtual void mouseReleaseEvent (QMouseEvent* event);
     virtual void mouseMoveEvent (QMouseEvent *event);
+
+    virtual void leaveEvent (QEvent *event);
+    virtual void enterEvent (QEvent *event);
+
 
     private:
 
@@ -63,6 +78,9 @@ class MapWidget : public QSFMLCanvas
     int m_x, m_y, m_w, m_h;
 
     QPoint m_highlightedCase;
+    bool m_mouseInside;
+
+    QPoint m_selectedCase;
 
     bool m_highlightEnabled;
 
@@ -86,6 +104,8 @@ class MapWidgetScroll : public QScrollArea
 {
     Q_OBJECT
 
+    friend class MapEditor;
+
     public:
     MapWidgetScroll(QWidget* parent=0);
     MapWidget* getMapWidget() { return &map; }
@@ -93,6 +113,7 @@ class MapWidgetScroll : public QScrollArea
     private slots:
 
     void ref();
+    void sizeUpdate();
 
     private:
     MapWidget map;
--- trunk/Client/release/moc_MapEditor.cpp
+++ trunk/Client/release/moc_MapEditor.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MapEditor.h'
 **
-** Created: Sat 29. Jan 11:47:58 2011
+** Created: Sat 29. Jan 19:05:57 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_MapEditor[] = {
        4,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -31,15 +31,32 @@ static const uint qt_meta_data_MapEditor[] = {
        0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
-      20,   16,   11,   10, 0x0a,
-      37,   10,   11,   10, 0x2a,
+      29,   16,   11,   10, 0x0a,
+      58,   54,   11,   10, 0x2a,
+      75,   10,   11,   10, 0x2a,
+      85,   10,   11,   10, 0x0a,
+     104,   95,   11,   10, 0x0a,
+     123,   10,   11,   10, 0x2a,
+     137,  135,   10,   10, 0x08,
+     159,  157,   10,   10, 0x08,
+     187,  179,   10,   10, 0x08,
+     209,  179,   10,   10, 0x08,
+     237,  235,   10,   10, 0x08,
+     264,   10,   10,   10, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_MapEditor[] = {
-    "MapEditor\0\0bool\0map\0loadMap(QString)\0"
-    "loadMap()\0"
+    "MapEditor\0\0bool\0map,ressPack\0"
+    "loadMap(QString,QString)\0map\0"
+    "loadMap(QString)\0loadMap()\0saveMap()\0"
+    "fileName\0saveMapAs(QString)\0saveMapAs()\0"
+    "x\0changeMapSizeX(int)\0y\0changeMapSizeY(int)\0"
+    "newCase\0changeCasePos(QPoint)\0"
+    "changeCurrentCase(QPoint)\0n\0"
+    "changeCurrentCaseRSID(int)\0"
+    "updateRessourcesList()\0"
 };
 
 const QMetaObject MapEditor::staticMetaObject = {
@@ -71,13 +88,27 @@ int MapEditor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
-        case 0: { bool _r = loadMap((*reinterpret_cast< QString(*)>(_a[1])));
+        case 0: { bool _r = loadMap((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2])));
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
-        case 1: { bool _r = loadMap();
+        case 1: { bool _r = loadMap((*reinterpret_cast< QString(*)>(_a[1])));
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 2: { bool _r = loadMap();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 3: { bool _r = saveMap();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 4: { bool _r = saveMapAs((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 5: { bool _r = saveMapAs();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 6: changeMapSizeX((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: changeMapSizeY((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: changeCasePos((*reinterpret_cast< QPoint(*)>(_a[1]))); break;
+        case 9: changeCurrentCase((*reinterpret_cast< QPoint(*)>(_a[1]))); break;
+        case 10: changeCurrentCaseRSID((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: updateRessourcesList(); break;
         default: ;
         }
-        _id -= 2;
+        _id -= 12;
     }
     return _id;
 }
